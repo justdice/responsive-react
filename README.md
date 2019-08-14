91b8197dd68d0a371e249501411be2dcb91311c7
sid <siddhartha@applike.info>
2019-08-14 15:27:28 +0200

    updated dimensions and added iPad pro

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # react-responsive
 conditional rendering of react component for responsive display
+
+# Demo: [Demo](https://codesandbox.io/s/goofy-mcnulty-ue7zq)
+
+<hr/>
+
+# Approach 1
+		
+		import { Responsive } from "react-responsive";
+
+		<>
+	    <Responsive displayIn={["Mobile", "Tablet"]}>
+	      <h2>This is a MOBILE/TABLET</h2>
+	    </Responsive>
+	    <Responsive displayIn={["Laptop"]}>
+	      <h2>This is a LAPTOP or a Larger screen</h2>
+	    </Responsive>
+    </>
+
+> Note: iPad pro in landscape is considered as a laptop screen here.
+
+# Approach 2
+
+		import {
+		  isMobileDevice,
+		  isTabletDevice,
+		  isLaptopDevice,
+		  isBiggerThanLaptop
+		} from "react-responsive/utilResponsive";
+
+		<>
+      {isMobileDevice() && <h2>This is a MOBILE</h2>}
+
+      {isTabletDevice() && <h2>This is a TABLET</h2>}
+
+      {isLaptopDevice() && <h2>This is a LAPTOP</h2>}
+
+      {isBiggerThanLaptop() && <h2>This is a LARGER SCREEN</h2>}
+    </>
+
+# Additional helper function:
+
+		import {getDeviceTypeInfo} from "react-responsive/utilResponsive";
+
+		// helper function
+		const {
+      width,
+      height,
+      deviceTypeVariant, 
+      deviceType,
+      orientation,
+      isFallback
+    } = getDeviceTypeInfo();
+
+    /*
+    	width = Width of viewport
+    	height = Height of viewport
+      deviceTypeVariant = [MobileSmall, MobileMedium, MobileLarge, Tablet, iPadPro, LaptopSmall, LaptopLarge, LargerThanLaptop, LargeScreenMax]
+      deviceType = [Laptop, Tablet, Mobile, LargerThanLaptop]
+      orientation = [Landscape, Portrait]
+      isFallback = [true, false] default false. true if it detects some awkward dimension (rare)
+    */
+
+
+
+edge cases: 
+1. Resolution 768 x 1024 is considered tablet (mostly iPads), while 1024 x 768 is macbook air
+2. iPad pro has large screen (1024 x 1366 portrait) compared to other tablets (when landscaped is as big as a midium sized laptop)
+
+Disclaimer: This plugin is resolution based and not device-type based. For sake of naming convension we are using the terms "Mobile", "Tablet", etc. Because
+of the edge cases explained above
--- src/Responsive.js
+++ src/Responsive.js
@@ -22,7 +22,11 @@ export class Responsive extends React.PureComponent {
     const { children, displayIn } = this.props;
     const { width, height } = this.state;
 
-    const shouldRenderChildren = this.shouldRender(displayIn, width, height);
+    const shouldRenderChildren = this.shouldRender(
+      displayIn.toString().toLowerCase(),
+      width,
+      height
+    );
 
     return (
       <React.Fragment>{shouldRenderChildren ? children : null}</React.Fragment>
@@ -36,30 +40,37 @@ export class Responsive extends React.PureComponent {
 
   shouldRender = (display, width, height) => {
     if (
-      display.indexOf("Laptop") !== -1 &&
+      display.indexOf("laptop") !== -1 &&
+      width > height &&
       width >= IdDeviceBreakpointsByWidth.laptop_min
     ) {
+      //  always landscape
       return true;
     }
 
-    if (
-      display.indexOf("Tablet") !== -1 &&
-      (width <= IdDeviceBreakpointsByWidth.tablet_max &&
-        width >= IdDeviceBreakpointsByWidth.tablet_min)
-    ) {
-      return true;
+    if (display.indexOf("tablet") !== -1) {
+      if (
+        width <= IdDeviceBreakpointsByWidth.tablet_max &&
+        width >= IdDeviceBreakpointsByWidth.tablet_min
+      ) {
+        return true;
+      }
+      //  Cater iPad pro portrait (ONLY)
+      if (width === 1024 && height === 1366) {
+        return true;
+      }
     }
 
     // For mobile regardless of orientation
     if (
-      display.indexOf("Mobile") !== -1 &&
+      display.indexOf("mobile") !== -1 &&
       width <= IdDeviceBreakpointsByWidth.mobile_max
     ) {
       return true;
     }
 
     if (
-      display.indexOf("MobilePortrait") !== -1 &&
+      display.indexOf("mobileportrait") !== -1 &&
       (width <= IdDeviceBreakpointsByWidth.mobile_max &&
         height >= IdMobileHeight.mobileLandscape_max)
     ) {
@@ -67,7 +78,7 @@ export class Responsive extends React.PureComponent {
     }
 
     return !!(
-      display.indexOf("MobileLandScape") !== -1 &&
+      display.indexOf("mobilelandScape") !== -1 &&
       (width <= IdDeviceBreakpointsByWidth.mobile_max &&
         height <= IdMobileHeight.mobileLandscape_min)
     );
